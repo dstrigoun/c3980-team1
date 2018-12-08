fb7e6fbc424b6d6cb3e0bbef6cd2bbb94ad59fcb
Kieran Lee <kieranlee88@gmail.com>
2018-12-08 12:00:59 -0800

    removed more comments

--- Final_Project/Sender.cpp
+++ Final_Project/Sender.cpp
@@ -26,52 +26,5 @@ void sendFrameToPort(HANDLE portHandle, char* frame, size_t frameLen) {
 	if (!WriteFile(portHandle, frame, frameLen, &numBytesWritten, NULL)) {
 		exit(1);
 	}
-	//OVERLAPPED osWrite = { 0 };
-	//DWORD dwWritten;
-	//DWORD dwRes;
-
-	//// Create this write operation's OVERLAPPED structure's hEvent.
-	//osWrite.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-	//if (osWrite.hEvent == NULL) 
-	//{
-	//	//error in creating event
-	//}
-	//// Issue write.
-	//if (!WriteFile(portHandle, frame, frameLen, &dwWritten, &osWrite))
-	//{
-	//	if (GetLastError() != ERROR_IO_PENDING) 
-	//	{
-	//		// WriteFile failed, but isn't delayed. Report error and abort.
-	//	}
-	//	else
-	//	{
-	//		// Write is pending.
-	//		dwRes = WaitForSingleObject(osWrite.hEvent, INFINITE);
-	//	}
-	//		
-	//	switch (dwRes)
-	//	{
-	//		// OVERLAPPED structure's event has been signalled. 
-	//	case WAIT_OBJECT_0:
-	//		if (!GetOverlappedResult(portHandle, &osWrite, &dwWritten, FALSE))
-	//		{
-	//			//error
-	//		}
-	//		else
-	//		{
-	//			// Write operation completed successfully.
-	//		}
-	//		break;
-	//	default:
-	//		// An error has occurred in WaitForSingleObject.
-	//		// This usually indicates a problem with the
-	//	   // OVERLAPPED structure's event handle.
-	//		break;
-	//	}
-	//}
-	//else 
-	//{
-	//	// WriteFile completed immediately.
-	//}
- //  CloseHandle(osWrite.hEvent);
+	
 }
